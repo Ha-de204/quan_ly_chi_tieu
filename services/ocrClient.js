0e2d382098730d640f0d37b5401ab2e35b0cbfd9
Ha Nguyen <ngha17012004@gmail.com>
2026-05-09 20:45:43 +0700

    fix ocr

---
+++ services/ocrClient.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const FormData = require('form-data');
+
+const OCR_SERVER_URL = "http://127.0.0.1:8000/scan";
+
+const sendToOCRServer = async (fileBuffer, filename) => {
+    try {
+        const form = new FormData();
+
+        form.append("file", fileBuffer, {
+            filename: filename || "image.jpg",
+            contentType: "image/jpeg",
+        });
+
+        const response = await axios.post(OCR_SERVER_URL, form, {
+            headers: {
+                ...form.getHeaders(),
+            },
+            timeout: 60000,
+        });
+
+        return response.data;
+
+    } catch (error) {
+        console.error("OCR Server Error:", error.message);
+        throw error;
+    }
+};
+
+module.exports = { sendToOCRServer };
