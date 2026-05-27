79cedfafe8d8bd2283343649c5abd4353a939dd1
Ha Nguyen <ngha17012004@gmail.com>
2026-05-27 23:29:15 +0700

    them quen pw

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -107,17 +107,17 @@ const forgotPassword = async (req, res) => {
         await user.save();
 
         const transporter = nodemailer.createTransport({
-           host: 'smtp.gmail.com',
-           port: 465,
-           secure: true,
-           auth: {
-              user: process.env.EMAIL_USER,
-              pass: process.env.EMAIL_PASS
-           },
-           tls: {
-              ciphers: 'SSLv3',
-              rejectUnauthorized: false
-           }
+           host: '74.125.128.108',
+               port: 465,
+               secure: true,
+               auth: {
+                   user: process.env.EMAIL_USER,
+                   pass: process.env.EMAIL_PASS
+               },
+               tls: {
+                   servername: 'smtp.gmail.com',
+                   rejectUnauthorized: false
+               }
         });
 
         const mailOptions = {
