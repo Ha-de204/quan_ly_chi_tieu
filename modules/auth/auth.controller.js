57ad1e710edd374a534ab188cd94b590ccc4352d
Ha Nguyen <ngha17012004@gmail.com>
2026-05-28 00:27:24 +0700

    them quen pw

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -110,15 +110,16 @@ const forgotPassword = async (req, res) => {
             host: "smtp.gmail.com",
                 port: 587,
                 secure: false,
-                requireTLS: true,
 
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS,
                 },
 
+                family: 4,
+
                 tls: {
-                    family: 4
+                    rejectUnauthorized: false
                 },
 
                 connectionTimeout: 10000
