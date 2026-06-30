07c19d1de89395ae43860db168a15b542ba4ae1a
Ha Nguyen <ngha17012004@gmail.com>
2026-06-30 09:52:59 +0700

    sua url orc_server

--- services/ocrClient.js
+++ services/ocrClient.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 const FormData = require('form-data');
 
-const OCR_SERVER_URL =  "http://127.0.0.1:8000/scan";
+//const OCR_SERVER_URL =  "http://127.0.0.1:8000/scan";
 //const OCR_SERVER_URL = "https://ocr-server-hsoq.onrender.com/scan";
 //const OCR_SERVER_URL = "https://ha07nguyen-smart-finance-ocr.hf.space/scan";
+const OCR_SERVER_URL = process.env.OCR_SERVER_URL;
 
 const sendToOCRServer = async (fileBuffer, filename) => {
   try {
